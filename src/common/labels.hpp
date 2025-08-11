682bc2702792049b9d137860ab90f45e5dc2957e
Electron7-7 <julian@frattarola.net>
2025-08-11 14:02:37 -0400

    checkpoint(laptop): Worked on better printouts and more efficient code on `08-11-25`

--- src/common/labels.hpp
+++ src/common/labels.hpp
@@ -8,25 +8,25 @@
 #define LABEL_WARN  "[WARNING]"
 #define LABEL_DEBUG "[DEBUG]"
 
-static const char* ERROR()
+static std::string ERROR()
 {
     if(Flags::NoColor.IsActive())
         return LABEL_ERROR;
-    return COLOR_BOLD(RED) LABEL_ERROR;
+    return COLOR_BOLD(RED) + LABEL_ERROR;
 }
 
-static const char* WARN()
+static std::string WARN()
 {
     if(Flags::NoColor.IsActive())
         return LABEL_WARN;
-    return COLOR_BOLD(YELLOW) LABEL_WARN;
+    return COLOR_BOLD(YELLOW) + LABEL_WARN;
 }
 
-static const char* DEBUG()
+static std::string DEBUG()
 {
     if(Flags::NoColor.IsActive())
         return LABEL_DEBUG;
-    return COLOR_BOLD(DEFAULT) LABEL_DEBUG COLOR_RESET;
+    return COLOR_BOLD(DEFAULT) + LABEL_DEBUG + COLOR_RESET;
 }
 
 static const char* RESET_COLOR()
--- src/main/output_file.cpp
+++ src/main/output_file.cpp
@@ -7,64 +7,80 @@
 #include <string>
 #include <filesystem>
 #include <fstream>
-#include <print>
 
-FileStatus CheckFilePath(const std::string& wish_file_name = "")
+FileStatus CheckFilePath(const std::string& wish_file_name)
 {
     if(wish_file_name.empty())
         return FileStatus::FAILURE;
 
-    // TODO: remove the std::filesystem code, as the filestreams will fail if the directory doesn't exist, anyways (i think...)
-    std::filesystem::path wish_filepath = std::filesystem::absolute(std::filesystem::path(wish_file_name));
+    std::filesystem::path wish_filepath  = std::filesystem::absolute(std::filesystem::path(wish_file_name));
+    std::filesystem::path wish_directory = std::filesystem::path(wish_filepath).remove_filename();
 
-    if(exists(wish_filepath.remove_filename()))
+    if(!exists(wish_directory))
     {
-        std::ifstream file_already_exists(wish_filepath.string());
-        if(file_already_exists)
-        {
-            file_already_exists.close();
-            return FileStatus::SUCCESS_FILE_EXISTS;
-        }
-
-        std::ofstream can_write_to_file(wish_filepath.string());
-        if(can_write_to_file)
-        {
-            can_write_to_file.close();
-            return FileStatus::SUCCESS_FILE_CREATED;
-        }
+        PRINT_MESSAGE("{} Directory '{}' does not exist{}\n", WARN(), wish_filepath.string(), COLOR_RESET)
+        return FileStatus::FAILURE;
     }
 
+    std::ifstream file_already_exists(wish_filepath);
+    if(file_already_exists)
+    {
+        if(Flags::Verbose.IsActive())
+            PRINT_OUT("::File '{}' is accessible\n", wish_filepath.generic_string())
+        file_already_exists.close();
+        return FileStatus::SUCCESS_FILE_EXISTS;
+    }
+
+    std::ofstream can_write_to_file(wish_filepath);
+    if(can_write_to_file)
+    {
+        if(Flags::Verbose.IsActive())
+            PRINT_OUT("::File '{}' can be created\n", wish_filepath.string())
+        can_write_to_file.close();
+        return FileStatus::SUCCESS_FILE_CREATED;
+    }
+
+    if(Flags::Verbose.IsActive())
+    {
+        PRINT_MESSAGE("{} Failed to access/create file at '{}'{}\n", WARN(), wish_filepath.string(), COLOR_RESET)
+        PRINTOUT("\t::This is usually due to insufficient permissions. Make sure you know what you're doing before you use sudo...")
+    }
     return FileStatus::FAILURE;
 }
 
-
-bool try_SetOutputFile(const char* wish_output_file, bool suppress_printouts)
+bool try_SetOutputFile(const char* wish_output_file)
 {
+    // FIXME: This 'NULL' check never happens, now, because I do that check in the main function
+
+    if(wish_output_file == NULL)
+    {
+        PRINT_DEBUG("'wish_output_file' is {}NULL{}; this is due to the environment variable '{}' not being set\n", COLOR_BOLD(RED), constant_ConfigFileLocationEnvironmentVariable, COLOR_RESET)
+        PRINT_DEBUG("\t- This isn't a big deal, btw, it just means that the program will use '{}' for the default location instead\n", constant_DefaultOutputFile)
+        return false;
+    }
+
+    PRINT_DEBUG("'wish_output_file' is '{}'\n", wish_output_file)
+
     FileStatus file_status = CheckFilePath(wish_output_file);
     const char* new_output_file;
 
     switch(file_status)
     {
     case FileStatus::FAILURE:
-        if(!suppress_printouts && ALLOW_MESSAGE())
-            std::print("{} Cannot write output to '{}'{}\n", WARN(), wish_output_file, RESET_COLOR());
-        new_output_file = getenv(constant_ConfigFileLocationEnvironmentVariable);
-        if(!new_output_file)
-            try_SetOutputFile(Options::Output.GetValue(), suppress_printouts);
+        PRINT_MESSAGE("{} Cannot write output to '{}'{}\n", WARN(), wish_output_file, RESET_COLOR())
         return false;
 
     // TODO: Make these cases better
     case FileStatus::SUCCESS_FILE_EXISTS:
-        Options::Output.SetValue(wish_output_file);
-        if(!suppress_printouts && ALLOW_PRINTOUT())
-            std::print("::Output will be appended to {}'{}'{}\n", COLOR(CYAN), wish_output_file, RESET_COLOR());
-        return true;
+        PRINT_OUT("::Output will be appended to {}'{}'{}\n", COLOR(CYAN), wish_output_file, RESET_COLOR())
+        break;
 
     case FileStatus::SUCCESS_FILE_CREATED:
-        Options::Output.SetValue(wish_output_file);
-        if(!suppress_printouts && ALLOW_PRINTOUT())
-            std::print("::Output will be written to {}'{}'{}\n", COLOR(CYAN), wish_output_file, RESET_COLOR());
-        return true;
+        PRINT_OUT("::Output will be written to {}'{}'{}\n", COLOR(CYAN), wish_output_file, RESET_COLOR())
+        break;
     }
+
+    Options::Output.SetValue(wish_output_file);
+    return true;
 }
 
