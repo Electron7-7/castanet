19197f179f95856a210dfcf7af41da923a6df40c
Electron7-7 <julian@frattarola.net>
2025-08-11 16:18:08 -0400

    optimization: Cut down 'try_SetOutputFile' to just a single "if" statement to check for file failure and a ternary operator to change the printout wording (not technically necessary but a nice touch, I think)

--- src/main/output_file.cpp
+++ src/main/output_file.cpp
@@ -50,37 +50,21 @@ FileStatus CheckFilePath(const std::string& wish_file_name)
 
 bool try_SetOutputFile(const char* wish_output_file)
 {
-    // FIXME: This 'NULL' check never happens, now, because I do that check in the main function
-
-    if(wish_output_file == NULL)
-    {
-        PRINT_DEBUG("'wish_output_file' is {}NULL{}; this is due to the environment variable '{}' not being set\n", COLOR_BOLD(RED), constant_ConfigFileLocationEnvironmentVariable, COLOR_RESET)
-        PRINT_DEBUG("\t- This isn't a big deal, btw, it just means that the program will use '{}' for the default location instead\n", constant_DefaultOutputFile)
-        return false;
-    }
-
     PRINT_DEBUG("'wish_output_file' is '{}'\n", wish_output_file)
 
     FileStatus file_status = CheckFilePath(wish_output_file);
-    const char* new_output_file;
 
-    switch(file_status)
+    if(file_status == FileStatus::FAILURE)
     {
-    case FileStatus::FAILURE:
         PRINT_MESSAGE("{} Cannot write output to '{}'{}\n", WARN(), wish_output_file, RESET_COLOR())
         return false;
-
-    // TODO: Make these cases better
-    case FileStatus::SUCCESS_FILE_EXISTS:
-        PRINT_OUT("::Output will be appended to {}'{}'{}\n", COLOR(CYAN), wish_output_file, RESET_COLOR())
-        break;
-
-    case FileStatus::SUCCESS_FILE_CREATED:
-        PRINT_OUT("::Output will be written to {}'{}'{}\n", COLOR(CYAN), wish_output_file, RESET_COLOR())
-        break;
     }
 
     Options::Output.SetValue(wish_output_file);
+
+    const char* written_or_appended = (file_status == FileStatus::SUCCESS_FILE_EXISTS) ? "appended" : "written";
+    PRINT_OUT("::Output will be {} to {}'{}'{}\n", COLOR(CYAN), written_or_appended, wish_output_file, RESET_COLOR())
+
     return true;
 }
 
